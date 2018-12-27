c8476b66ce4f5ab7caba22549602cf83d1693cb1
Robert Herber <rwherber@gmail.com>
2018-12-27 14:02:17 -0800

    Added GridPosition instead of using Vector2

---
+++ GridPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPosition  {
+	/*
+	 * This class simply keeps the position of a grid cell in a readable manner. Similiar to a vector2, but without the extra functions and instead of x and y floats, we have row
+	 * and column ints.
+	 */
+
+	public int row;
+	public int column;
+
+
+	/// <summary>
+	/// Create a new GridPosition with passed in row and column values.
+	/// </summary>
+	/// <param name="row">The row.</param>
+	/// <param name="column">The column.</param>
+	public GridPosition(int row, int column)
+	{
+		this.row = row;
+		this.column = column;
+	}
+}
